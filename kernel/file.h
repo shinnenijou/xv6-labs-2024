8a1c788387c876b53f9b977c145b0a40a6ac6010
shinnenijou <littlesword111@gmail.com>
2025-04-12 00:05:17 +0800

    Student solution: Part 1 Large files, fix wrong array length in in-memory inode definition

--- kernel/file.h
+++ kernel/file.h
@@ -26,7 +26,7 @@ struct inode {
   short minor;
   short nlink;
   uint size;
-  uint addrs[NDIRECT+NDIRECT+NINDIRECTADDR+NDINDIRECTADDR];
+  uint addrs[NDIRECT+NINDIRECTADDR+NDINDIRECTADDR];
 };
 
 // map major device number to device functions.
